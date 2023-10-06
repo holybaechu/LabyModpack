a42c07a338f39b59a4ec145d0f6480402891bee6
Bae Joon Hoo <joonhoo0524@naver.com>
2023-10-06 23:22:03 +0900

    0.0.3

--- scripts/upload-curseforge.js
+++ scripts/upload-curseforge.js
@@ -22,7 +22,7 @@ async function getVersionIdByVersion(mc_version){
     })
 
     let versionTypeId = versionTypes.data[0].id
-    let formattedVersion = `minecraft-${mc_version.split('.')[0]}-${mc_version.split('.')[1]}`
+    let formattedVersion = `${mc_version.split('.')[0]}-${mc_version.split('.')[1]}`
     for (let versionType of versions.data){
         if (versionType.slug == formattedVersion){
             versionTypeId = versionType.id
